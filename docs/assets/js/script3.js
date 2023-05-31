637f1791147b02e8da166db3e9d0beb3c17d317e
EunhyeonKang <rkddmsgus1@daum.net>
2023-06-01 01:33:19 +0900

    ui 완성

--- docs/assets/js/script3.js
+++ docs/assets/js/script3.js
@@ -1,159 +1,217 @@
-function initMap() {
-    const input = document.getElementById('search-input');
-    const autocomplete = new google.maps.places.Autocomplete(input);
-
-    autocomplete.addListener('place_changed', function() {
-      const place = autocomplete.getPlace();
-      if (!place.geometry) {
-        // 잘못된 주소나 검색 결과가 없을 경우 처리할 로직 작성
-        return;
-      }
-
-      // 주소의 위치 좌표 정보
-      const latitude = place.geometry.location.lat();
-      const longitude = place.geometry.location.lng();
-
-      // 검색 결과 출력
-      const result = document.getElementById('result');
-      result.textContent = `입력한 주소의 위치 좌표: ${latitude}, ${longitude}`;
-    });
+// 드래그 시작 시 호출되는 함수
+function dragStart(event) {
+    draggedElement = event.target;
+    prevIndex = Array.from(draggedElement.parentNode.children).indexOf(draggedElement);
+    event.dataTransfer.effectAllowed = "move";
+    event.dataTransfer.setData("text/html", draggedElement.innerHTML);
+    draggedElement.classList.add("dragging");
   }
-
-  // Google Maps API 스크립트 로드 후 initMap 함수 호출
-  window.initMap = initMap;
+  
+  // 날짜 클릭 시 동작
+function onDateClick(event) {
+    var selectedDates = this.calendarElement.querySelectorAll('.selected');
+    selectedDates.forEach(function(date) {
+      date.classList.remove('selected');
+    });
+  
+    var selectedDay = event.target.textContent;
+    console.log('Selected day:', selectedDay);
+    event.target.classList.add('selected');
+    // 여기에 선택한 날짜에 대한 동작을 추가할 수 있습니다.
+  }
+  
 
   var Calendar = {
-  // 현재 날짜 정보
-  currentDate: new Date(),
-
-  // 캘린더 테이블을 표시할 요소
-  calendarElement: null,
-
-  // 캘린더 테이블 생성 및 초기화
-  createCalendar: function (element) {
+    // 현재 날짜 정보
+    currentDate: new Date(),
+  
+    // 캘린더 테이블을 표시할 요소
+    calendarElement: null,
+  
+    // 선택된 날짜 배열
+    selectedDates: [],
+  
+    // 캘린더 테이블 생성 및 초기화
+    createCalendar: function (element) {
       this.calendarElement = element;
       this.updateCalendar();
-  },
-
-  // 이전 달로 이동
-  goToPreviousMonth: function () {
+    },
+  
+    // 이전 달로 이동
+    goToPreviousMonth: function () {
       this.currentDate.setMonth(this.currentDate.getMonth() - 1);
       this.updateCalendar();
-  },
-
-  // 다음 달로 이동
-  goToNextMonth: function () {
+    },
+  
+    // 다음 달로 이동
+    goToNextMonth: function () {
       this.currentDate.setMonth(this.currentDate.getMonth() + 1);
       this.updateCalendar();
-  },
-
-  // 날짜 클릭 시 동작
-  onDateClick: function (event) {
-      var selectedDates = this.calendarElement.querySelectorAll('.selected');
-      selectedDates.forEach(function(date) {
-          date.classList.remove('selected');
-      });
-
-      var selectedDay = event.target.textContent;
-      console.log('Selected day:', selectedDay);
-      event.target.classList.add('selected');
-      // 여기에 선택한 날짜에 대한 동작을 추가할 수 있습니다.
-  },
-
-  // 캘린더 업데이트
-  updateCalendar: function () {
+    },
+  
+    // 캘린더 업데이트
+    updateCalendar: function () {
       var year = this.currentDate.getFullYear();
       var month = this.currentDate.getMonth();
-
+  
       var firstDay = new Date(year, month, 1);
       var lastDay = new Date(year, month + 1, 0);
-
+  
       var firstDayOfWeek = firstDay.getDay();
       var daysInMonth = lastDay.getDate();
-
+  
       var calendarTable = document.createElement('table');
-
+  
       // 월, 연도 표시
       var caption = document.createElement('caption');
       caption.textContent = year + '년 ' + (month + 1) + '월';
       calendarTable.appendChild(caption);
-
+  
       // 이전 달로 이동 버튼
       var previousButton = document.createElement('button');
-      previousButton.textContent = '이전 달';
+      previousButton.innerHTML = '이전 달';
+      previousButton.classList.add('arrow-button');
       previousButton.addEventListener('click', function () {
-          Calendar.goToPreviousMonth();
+        Calendar.goToPreviousMonth();
+        enableDrag(); // 드래그 기능 활성화 함수 호출
       });
       calendarTable.appendChild(previousButton);
-
-      // 다음 달로 이동 버튼
-      var nextButton = document.createElement('button');
-      nextButton.textContent = '다음 달';
-      nextButton.addEventListener('click', function () {
-          Calendar.goToNextMonth();
-      });
-      calendarTable.appendChild(nextButton);
-
+  
+        // 다음 달로 이동 버튼
+        var nextButton = document.createElement('button');
+        nextButton.innerHTML = '다음 달';
+        nextButton.classList.add('arrow-button');
+        nextButton.addEventListener('click', function () {
+        Calendar.goToNextMonth();
+        // 다음 달로 이동한 후에 드래그 기능을 다시 활성화해야 합니다.
+        enableDrag(); // 드래그 기능 활성화 함수 호출
+        });
+        calendarTable.appendChild(nextButton);
+        
       // 요일 표시
       var weekdays = ['일', '월', '화', '수', '목', '금', '토'];
       var weekdaysRow = document.createElement('tr');
       for (var i = 0; i < 7; i++) {
-          var th = document.createElement('th');
-          th.textContent = weekdays[i];
-          weekdaysRow.appendChild(th);
+        var th = document.createElement('th');
+        th.textContent = weekdays[i];
+        weekdaysRow.appendChild(th);
       }
       calendarTable.appendChild(weekdaysRow);
-
+  
       // 날짜 표시
       var date = 1;
       var daysRow = document.createElement('tr');
-
+  
       // 이전 달 날짜 표시
       for (var i = 0; i < firstDayOfWeek; i++) {
-          var td = document.createElement('td');
-          td.classList.add('previous-month');
-          td.textContent = lastDay.getDate() - (firstDayOfWeek - i - 1);
-          daysRow.appendChild(td);
+        var td = document.createElement('td');
+        td.classList.add('previous-month');
+        td.textContent = lastDay.getDate() - (firstDayOfWeek - i - 1);
+        td.addEventListener('dragstart', dragStart.bind(this)); // 수정된 부분
+        td.addEventListener('click', onDateClick.bind(this)); // 수정된 부분
+        daysRow.appendChild(td);
       }
-
+  
       // 현재 달 날짜 표시
       while (date <= daysInMonth) {
-          var td = document.createElement('td');
-          td.textContent = date;
-          if (
-              date === this.currentDate.getDate() &&
-              month === this.currentDate.getMonth() &&
-              year === this.currentDate.getFullYear()
-          ) {
-              td.classList.add('selected');
-          }
-          td.addEventListener('click', this.onDateClick.bind(this));
-          daysRow.appendChild(td);
-          if (daysRow.children.length % 7 === 0) {
-              calendarTable.appendChild(daysRow);
-              daysRow = document.createElement('tr');
-          }
-          date++;
+        var td = document.createElement('td');
+        td.textContent = date;
+        if (
+          date === this.currentDate.getDate() &&
+          month === this.currentDate.getMonth() &&
+          year === this.currentDate.getFullYear()
+        ) {
+          td.classList.add('selected');
+        }
+        td.addEventListener('click', onDateClick.bind(this));
+        td.addEventListener('dragstart', dragStart.bind(this));
+        td.addEventListener('dragstart', dragStart.bind(this)); // 수정된 부분
+        td.addEventListener('click', onDateClick.bind(this)); // 수정된 부분
+        daysRow.appendChild(td);
+        if (daysRow.children.length % 7 === 0) {
+          calendarTable.appendChild(daysRow);
+          daysRow = document.createElement('tr');
+        }
+        date++;
       }
-
+  
       // 다음 달 날짜 표시
       var nextMonthDay = 1;
       while (daysRow.children.length < 7) {
-          var td = document.createElement('td');
-          td.classList.add('next-month');
-          td.textContent = nextMonthDay;
-          daysRow.appendChild(td);
-          nextMonthDay++;
+        var td = document.createElement('td');
+        td.classList.add('next-month');
+        td.textContent = nextMonthDay;
+        td.addEventListener('dragstart', dragStart.bind(this)); // 수정된 부분
+        td.addEventListener('click', onDateClick.bind(this)); // 수정된 부분
+        daysRow.appendChild(td);
+        nextMonthDay++;
       }
-
+  
       calendarTable.appendChild(daysRow);
-
+  
       // 캘린더 요소에 테이블 추가
       this.calendarElement.innerHTML = '';
       this.calendarElement.appendChild(calendarTable);
-  },
+    },
+  };
+  
+  
+  var isDragging = false;
+  var startDateElement = null;
+  function enableDrag() {
+
+  var dateElements = calendarElement.getElementsByTagName('td');
+  
+  Array.from(dateElements).forEach(function(dateElement) {
+    dateElement.addEventListener('mousedown', function(event) {
+      event.preventDefault();
+      isDragging = true;
+      startDateElement = event.target;
+      startDateElement.classList.add("selected");
+    });
+  
+    dateElement.addEventListener('mouseover', function(event) {
+      if (isDragging) {
+        var endDateElement = event.target;
+        if (endDateElement !== startDateElement) {
+          var allDateElements = Array.from(dateElements);
+          var startIndex = allDateElements.indexOf(startDateElement);
+          var endIndex = allDateElements.indexOf(endDateElement);
+          var minIndex = Math.min(startIndex, endIndex);
+          var maxIndex = Math.max(startIndex, endIndex);
+  
+          allDateElements.forEach(function(element, index) {
+            if (index >= minIndex && index <= maxIndex) {
+              element.classList.add("selected");
+            } else {
+              element.classList.remove("selected");
+            }
+          });
+        }
+      }
+    });
+  
+    dateElement.addEventListener('mouseup', function(event) {
+      isDragging = false;
+      startDateElement = null;
+  
+      var selectedDates = calendarElement.querySelectorAll('.selected');
+  
+      var dateArray = Array.from(selectedDates).map(function(dateElement) {
+        return dateElement.textContent;
+      });
+  
+      console.log('Selected dates:', dateArray);
+  
+    //   selectedDates.forEach(function(date) {
+    //     date.classList.remove('selected');
+    //   });
+    });
+  });
 };
 
-// 캘린더 생성
-var calendarElement = document.getElementById('calendar');
-Calendar.createCalendar(calendarElement);
+  // 캘린더 생성
+  var calendarElement = document.getElementById('calendar');
+  Calendar.createCalendar(calendarElement);
+  enableDrag(); // 초기에 드래그 기능 활성화
+
--- src/assets/js/script4.js
+++ src/assets/js/script4.js
@@ -21,3 +21,132 @@ xhr.onreadystatechange = function() {
 };
 
 xhr.send();
+
+
+const cardContainer = document.querySelector('.card-container');
+
+fetch('./data/card.json')
+  .then(response => response.json())
+  .then(data => {
+    data.cards.forEach(card => {
+      const cardElement = document.createElement('div');
+      cardElement.classList.add('card');
+
+      const imageElement = document.createElement('img');
+      imageElement.src = card.image;
+      cardElement.appendChild(imageElement);
+
+      const nameElement = document.createElement('h3');
+      nameElement.textContent = card.name;
+      cardElement.appendChild(nameElement);
+
+      const typeElement = document.createElement('p');
+      typeElement.textContent = `Type: ${card.type}`;
+      cardElement.appendChild(typeElement);
+
+      if (card.creditLimit) {
+        const creditLimitElement = document.createElement('p');
+        creditLimitElement.textContent = `Credit Limit: $${card.creditLimit}`;
+        cardElement.appendChild(creditLimitElement);
+      }
+
+      const balanceElement = document.createElement('p');
+      balanceElement.textContent = `Balance: $${card.balance}`;
+      cardElement.appendChild(balanceElement);
+
+      cardContainer.appendChild(cardElement);
+    });
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
+fetch('./data/traffic_data.json')
+  .then(response => response.json())
+  .then(data => {
+    const tableBody = document.querySelector('#trafficTable tbody');
+
+    data.traffic.forEach(item => {
+      const row = document.createElement('tr');
+
+      const idCell = document.createElement('td');
+      idCell.textContent = item.id;
+      row.appendChild(idCell);
+
+      const locationCell = document.createElement('td');
+      locationCell.textContent = item.location;
+      row.appendChild(locationCell);
+
+      const vehicleCountCell = document.createElement('td');
+      vehicleCountCell.textContent = item.vehicleCount;
+      row.appendChild(vehicleCountCell);
+
+      const statusCell = document.createElement('td');
+      statusCell.textContent = item.status;
+      row.appendChild(statusCell);
+
+      tableBody.appendChild(row);
+    });
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
+// API 요청
+/*
+fetch('https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=4Xq9MUHZEaIYSBNn1XcmpLC3psy0AhQZ&searchdate=20230431&data=AP01', { 
+  headers: {
+    'Access-Control-Allow-Origin': '*'
+  }
+})
+.then(response => {
+    console.log(response);
+  })
+  .catch(error => {
+    // 에러 처리
+    console.error(error);
+  });*/
+  fetch('https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=4Xq9MUHZEaIYSBNn1XcmpLC3psy0AhQZ&searchdate=20230431&data=AP01', { 
+  mode: 'no-cors' 
+})
+  .then(response => {
+    console.log(response)
+  })
+  .catch(error => {
+    // 에러 처리
+  });
+
+  function loginWithKakao() {
+    Kakao.Auth.authorize({
+    redirectUri: 'http://127.0.0.1:5500/kakao/login.html',
+    });
+}
+
+// 아래는 데모를 위한 UI 코드입니다.
+displayToken();
+function displayToken() {
+    var token = getCookie('authorize-access-token');
+
+    if(token) {
+        Kakao.Auth.setAccessToken(token);
+        Kakao.Auth.getStatusInfo()
+            .then(function(res) {
+            if (res.status === 'connected') {
+                document.getElementById('token-result').innerText
+                = 'login success, token: ' + Kakao.Auth.getAccessToken();
+            }
+            })
+            .catch(function(err) {
+            Kakao.Auth.setAccessToken(null);
+            });
+    }
+}
+
+function getCookie(name) {
+    var parts = document.cookie.split(name + '=');
+    if (parts.length === 2) { 
+        return parts[1].split(';')[0]; 
+    }
+}
